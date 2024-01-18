244d4aeff8e9d0c13fa5778ea5dd1b3ab4f48d4d
Arsenka <1234xl.com@gmail.com>
2024-01-18 17:25:42 +0300

    Updated EmailForm.css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 
 import "./styles/styleguide.css";
-// import Login from "./pages/Login/Login.jsx";
-import ForgotPassword from "./pages/ForgotPassword/ForgotPassword.jsx";
-
+import Login from "./pages/Login/Login.jsx";
+// import ForgotPassword from "./pages/ForgotPassword/ForgotPassword.jsx";
 // import Register from "./pages/Register/Register.jsx"
+
 
 function App() {
 
     return (
         <>
-            <ForgotPassword/>
-            {/*<Login/>*/}
+            {/*<ForgotPassword/>*/}
+            <Login/>
             {/*  <Register/>*/}
         </>
     )
