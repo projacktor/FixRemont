dc8d0e2f15fb56363d88d6a5360d6d383d24688f
Arsenka <1234xl.com@gmail.com>
2024-01-18 17:02:34 +0300

    Create ForgotPassword page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 
-import "./styles/styleguide.css";
-import Login from "./pages/Login/Login.jsx";
+import {ForgotPassword} from "./pages/ForgotPassword/ForgotPassword.jsx";
+// import Login from "./pages/Login/Login.jsx";
 // import {Register} from "./pages/Register/Register.jsx"
+import "./styles/styleguide.css";
+
 
 function App() {
 
   return (
     <>
-      <Login/>
+      {/*<Login/>*/}
       {/*  <Register/>*/}
+        <ForgotPassword/>
     </>
   )
 }
